3da9d7bf86cbe36d58f16b6f3f13e19de24660b4
CLL <140147262+CLcosep@users.noreply.github.com>
2023-10-31 11:40:51 +0800

    Edited comments in play.js

--- play.js
+++ play.js
@@ -1,16 +1,15 @@
-//create a variable that takes user input via prompt
+// Rock paper scissor game
 
-//create a variable that stores the result of the function randomGenerate
-
-//create a function named ranomGenerate that randomly selects rock,paper,scissor
+//This function will randomly pick items in the array to be used in the next function
 function randomGenerate(){
     let arr = ["rock", "paper", "scissors"];
     let ranResult = arr[(Math.floor(Math.random() * arr.length))];
     return ranResult;
 }
-// create a function called playRound() that takes 2 parameters the user input and the computer generated input
+//This function will compare playerSelection and computerSelection
+//And returns the result as well as an indicator if the player won or lost
 function playRound(playerSelection, computerSelection){
-    let flag = 0; // 0 if player losses 1 if won
+    let flag = 0; // 0 = loss, 1 = won, 2 = tie
     let playResult;
     if (playerSelection === computerSelection){
         playResult = `its a tie both are ${playerSelection}`;
@@ -25,14 +24,13 @@ function playRound(playerSelection, computerSelection){
     } else {
         playResult = `computer won using ${computerSelection} against your ${playerSelection}`;
     }
+
     return {
         flag, 
         playResult
     };
 }
-//the function playRound() will compare the two parameters and return the result 
-
-// finally create a game() function that uses the previous function top play a 5 round game
+//This function will call playRound() and keeps the score of the player in each round 
 function game(){
     let i = 1;
     let playerScore = 0;
@@ -50,9 +48,9 @@ function game(){
             computerScore++;
         }
         console.log(`Round ${i}: ${result.playResult}`);
-        
         i++;
     }
+
     if (playerScore > computerScore){
         console.log(`${playerScore} :: ${computerScore}, Player Won!!!!`);
     } else if (playerScore === computerScore){
@@ -62,23 +60,16 @@ function game(){
     }
 
 }
-// that keeps score and reports a winner or loser at the end
+
 game();
 
-/*let playerScore = 0;
-    let computerScore = 0;
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result.playResult);
-    if(result.flag === 2){
-        computerScore++;
-        playerScore++;
-    } else if(result.flag === 1){
-        playerScore++;
-    } else {
-        computerScore++;
-    }
-    console.log(result.flag);
-    console.log(`playerscore: ${playerScore}`);
-    console.log(`computerscore: ${computerScore}`);
-    */
-   
+/*
+    Algorithm: 
+    --create a variable that takes user input via prompt
+    --create a variable that stores the result of the function randomGenerate
+    --create a function named ranomGenerate that randomly selects rock,paper,scissor
+    --create a function called playRound() that takes 2 parameters the user input and the computer generated input
+    --the function playRound() will compare the two parameters and return the result 
+    --finally create a game() function that uses the previous function top play a 5 round game
+        that keeps score and reports a winner or loser at the end
+*/
